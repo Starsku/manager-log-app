55da70235a2c24564a6ab0e84d74e4f9d36a5e18
Starsku <110741843+Starsku@users.noreply.github.com>
2025-11-22 16:44:13 +0000

    fichier env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext' // Indispensable pour supporter import.meta.env sans erreur
+  }
 })
